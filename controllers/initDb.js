31587d80502f144caca16351e6f59bdb320e82a9
Arpita Mishra <arpitamishra30062005@gmail.com>
2026-07-31 00:13:08 +0530

    intalled the dependencies for frontend using vitelatest and added a basic boiler plate to app.jsx

---
+++ controllers/initDb.js
@@ -0,0 +1,25 @@
+const {query} = require('../models/connections.js');
+const initDatabase=async()=>{
+    const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS users (
+            id SERIAL ,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            registration_no CHAR(10) UNIQUE NOT NULL,
+            password VARCHAR(255) CHECK (length(password) > 8) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            age INT  CHECK (age BETWEEN 16 AND 65) NOT NULL,
+            PRIMARY KEY (id, registration_no)
+            
+        );
+        ` 
+try {
+    await query(createTableQuery);
+    console.log("Table is created Successfully");
+} catch (error) {
+    console.log(error);
+    process.exit(1);
+}
+}
+module.exports = {
+    initDatabase
+}
