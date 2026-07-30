8891f5cd47ad6921efda8087c05e1d5ed31a894b
Arpita Mishra <arpitamishra30062005@gmail.com>
2026-07-30 23:28:01 +0530

    implemented both the post methods  to add new user and for login

--- Backend/server.js
+++ Backend/server.js
@@ -20,6 +20,41 @@ app.get('/users', async(req, res) => {
 }
     }
 )
+
+app.post('/user', async(req, res) => {
+      const { username, registration_no, email, password,age} = req.body;
+        const insertUserQuery = `INSERT INTO users (username, registration_no, email, password, age) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+        try{
+            const result = await db.query(createUserQuery, [
+            username,
+            regustration_no,
+            email,
+            password,
+            age
+        ]);
+        res.status(201).json(result.rows[0]);}
+        catch (error) {
+        res.status(500).json({ error: 'Failed to create user' });
+
+        }
+   } )
+app.post('/login',async(req, res) => {
+    const { username, password } = req.body;
+    const getUserQuery = `SELECT * FROM users WHERE username = $1 AND password = $2`;
+    try{
+        const result = await db.query(getUserQuery, [
+            username,password
+        ]);
+        res.status(200).json(result.rows[0]);
+   
+ }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to login' });
+    }
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
