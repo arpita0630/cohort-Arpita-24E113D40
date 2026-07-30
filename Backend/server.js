940c532cf2b558deefacaf7b3a29a7e21a331b03
Arpita Mishra <arpitamishra30062005@gmail.com>
2026-07-30 23:24:53 +0530

    added the 1st get method for welcome  and 2nd  get method to get all the users

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,25 @@
 const  express = require("express");
 const app = express();
 const PORT = 3000;
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Welcome to User Management API'
+    })
+})
+app.get('/users', async(req, res) => {
+    const getUsersQuery = `SELECT * FROM users`;
+    try {
+        const result = await db.query(getUsersQuery);
+        res.status(200).json(result.rows)
+        
+    }
+
+    catch (error) {
+    res.status(500).json([]);
+}
+    }
+)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
